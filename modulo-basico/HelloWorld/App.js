f176f17221b554f8d6202bc8f86c60a7c290cc4a
Gabryel Boeira <GabryelBoeira@users.noreply.github.com>
2020-08-05 11:05:09 -0300

    Conceitos de componentição
    criação de componentes e funcionamento básico de inicilização de classe

---
+++ modulo-basico/HelloWorld/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { View, Text, Image } from 'react-native';
+
+class App extends Component{
+  render(){
+    let imagem = 'https://thumbs.dreamstime.com/b/imagem-de-fundo-bonita-do-c%C3%A9u-da-natureza-64743160.jpg';
+    
+    return (
+      <View>
+        <Text style={{color: '#fff', fontSize: 25, margin: 15}}>Hello World!</Text>       
+        <Imagem imagem={imagem}/>
+      </View>
+    );
+  }
+}
+
+class Imagem extends Component {
+  render() {
+    return (
+      <Image 
+        source={{uri: this.props.imagem }}
+        style={{width: 300, height: 300}}
+      />  
+    );
+  }
+}
+    
+export default App;
