aa86443cb58abbd6277fd7c7d63c123ff4a26958
Gabryel Boeira <GabryelBoeira@users.noreply.github.com>
2020-08-06 09:25:39 -0300

    Trabalhando com tamanhos fixos e dinamicos
    Utilização de tag para que o o proprio sistema possa gerenciar o tamanho de cada compomente

--- modulo-basico/HelloWorld/App.js
+++ modulo-basico/HelloWorld/App.js
@@ -3,46 +3,16 @@ import { View, Text, StyleSheet } from 'react-native';
 
 class App extends Component{
 
-  constructor(props){
-    super(props);
-    this.state = {
-      nome : 'Gabryel'
-    };
-
-    this.entrar = this.entrar.bind(this);
-  }
-
-  entrar(nome){
-    this.setState({
-      nome: nome
-    })
-  }
-
   render(){
     return (
-      <View style={styles.styleView}>
-        
-        <Text style={[styles.areaPrincipal, styles.centerText]}>  texto 1 </Text>  
-        <Text style={styles.centerText}>  texto 2 </Text>  
-        <Text> texto 3 </Text>  
-        <Text style={styles.areaPrincipal}>  texto 4 </Text>  
-
+      <View style={{flex:1, backgroundColor: '#00f'}}>
+          <View style={{width: 50, right:50, backgroundColor: '#f00'}}></View>
+           <View style={{flex:1, backgroundColor: '#e61'}}></View>
+           <View style={{flex:1, backgroundColor: '#fde'}}></View>
+           <View style={{flex:1, backgroundColor: '#000'}}></View>
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  styleView : {
-    marginTop: 40
-  },
-  areaPrincipal : {
-    fontSize : 40,
-    color : '#f10'
-  },
-  centerText : {
-    textAlign : 'center'
-  }
-});
 
 export default App;
