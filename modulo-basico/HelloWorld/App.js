30118c2149bb2c868e559daa7d298e6fdb93ccdb
Gabryel Boeira <GabryelBoeira@users.noreply.github.com>
2020-08-06 10:05:55 -0300

    Mais sobre Tipos Alinhamentos
    Formas de organizar a visualizção dos compomentes

--- modulo-basico/HelloWorld/App.js
+++ modulo-basico/HelloWorld/App.js
@@ -1,15 +1,32 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View , Text, StyleSheet } from 'react-native';
 
 class App extends Component{
   render(){
-    return (
-      <View style={{flex:1, flexDirection : 'column', justifyContent: 'center', alignItems: 'center'}}>
+    return ( 
+      <View style={{flex:1}}> 
+        <View style={{flex:1, flexDirection : 'column', justifyContent: 'center', alignItems: 'center'}}>
           <View style={{width: 50, height: 50, backgroundColor: '#f00'}}></View>
           <View style={{width: 50, height: 50, backgroundColor: '#e61'}}></View>
           <View style={{width: 50, height: 50, backgroundColor: '#fde'}}></View>
           <View style={{width: 50, height: 50, backgroundColor: '#000'}}></View>
-      </View>
+        </View>
+        <View style={{flex: 1, flexDirection: 'row', justifyContent:'space-between'}}>
+          <View style={{width: 50, height: 50, backgroundColor: 'green'}} />
+          <View style={{width: 50, height: 50, backgroundColor: 'blue'}} />
+          <View style={{width: 50, height: 50, backgroundColor: 'red'}} />
+        </View>
+        <View style={{flex:1, flexDirection : 'row',  justifyContent:'space-around'}}>
+          <View style={{width: 50, height: 50, backgroundColor: 'green'}} />
+          <View style={{width: 50, height: 50, backgroundColor: 'blue'}} />
+          <View style={{width: 50, height: 50, backgroundColor: 'red'}} />
+        </View>
+        <View style={{flex: 1, flexDirection: 'column', justifyContent: 'space-evenly'}}>
+          <View style={{width: 100, height: 50, backgroundColor: 'green'}} />
+          <View style={{height: 50, backgroundColor: 'blue'}} />
+          <View style={{height: 50, backgroundColor: 'red'}} />
+        </View>
+      </View>    
     );
   }
 }
