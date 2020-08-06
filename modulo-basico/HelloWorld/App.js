f920354b62accdfee748ac229012ac364d00f071
Gabryel Boeira <GabryelBoeira@users.noreply.github.com>
2020-08-06 09:39:47 -0300

    Trabalhando com FlexBox e alinhamentos
    Utilização e estudando comportamento das tags de de posicionamento dos compomentes para a tela do smartPhone

--- modulo-basico/HelloWorld/App.js
+++ modulo-basico/HelloWorld/App.js
@@ -2,14 +2,13 @@ import React, { Component } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
 class App extends Component{
-
   render(){
     return (
-      <View style={{flex:1, backgroundColor: '#00f'}}>
-          <View style={{width: 50, right:50, backgroundColor: '#f00'}}></View>
-           <View style={{flex:1, backgroundColor: '#e61'}}></View>
-           <View style={{flex:1, backgroundColor: '#fde'}}></View>
-           <View style={{flex:1, backgroundColor: '#000'}}></View>
+      <View style={{flex:1, flexDirection : 'column', justifyContent: 'center', alignItems: 'center'}}>
+          <View style={{width: 50, height: 50, backgroundColor: '#f00'}}></View>
+          <View style={{width: 50, height: 50, backgroundColor: '#e61'}}></View>
+          <View style={{width: 50, height: 50, backgroundColor: '#fde'}}></View>
+          <View style={{width: 50, height: 50, backgroundColor: '#000'}}></View>
       </View>
     );
   }
