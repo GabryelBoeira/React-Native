53d5347812dbb2817cd500f5847302f3e3de1abb
Gabryel Boeira <gabryel392@gmail.com>
2020-08-11 19:37:13 -0300

    trabalhando com Slider
    Utilizado o componente de Slider para utilização em casos de escolha númerica ex: avaliações de notas de 0 a 10

--- modulo-intermediario/projetoBase/App.js
+++ modulo-intermediario/projetoBase/App.js
@@ -1,43 +1,30 @@
 import React, { Component } from 'react'
 import { Text, StyleSheet, View} from 'react-native'
-import {Picker} from '@react-native-community/picker'
+import Slider from '@react-native-community/slider'
 
 export default class App extends Component {
   constructor(props){
     super(props);
     this.state = {
-      pizza: 0,
-      pizzas: [{key: 0, value: 29.99, label: "Frango/Catupiry"},
-               {key: 1, value: 34.99, label: "Aliche" },
-               {key: 2, value: 24.99, label: "Calabresa"},
-               {key: 3, value: 29.99, label: "Bacon e Milho"}]
+      valor: 50,
     };
 
-    this.pizzasItem = this.pizzasItem.bind(this);
   }
 
-  pizzasItem = () => {
-    return(
-     this.state.pizzas.map((item, index) => {
-      return <Picker.Item key={index} value={index} label={item.label} />;
-    }));
-  }
   render() {
-
     return (
       <View style={styles.container}>
-        <Text style={styles.logo}>Menu Pizza</Text>
-        <Picker
-            selectedValue={this.state.pizza}
-            onValueChange={pizza => this.setState({ pizza })}>
-            {this.pizzasItem()}                      
-        </Picker>
-
-        <Text style={styles.pizzas}>
-          Você Selecionou: {this.state.pizzas[this.state.pizza].label}
-        </Text>
-        <Text style={styles.pizzas}>
-          R$:  {this.state.pizzas[this.state.pizza].value.toFixed(2)}
+        <Slider
+          minimumValue={0}
+          maximumValue={100}
+          onValueChange={valor => this.setState({valor})}
+          value={this.state.valor}
+          minimumTrackTintColor="#00FF0F"
+          maximumTrackTintColor="#ff0f00"
+          onMagicTap={true}
+        />
+        <Text style={styles.text}> 
+          {this.state.valor.toFixed(0)}
         </Text>
       </View>
     );
@@ -49,14 +36,8 @@ const styles = StyleSheet.create({
     flex: 1,
     marginTop: 15
   },
-  logo: {
+  text: {
     textAlign: 'center',
-    fontSize: 25, 
-  },
-  pizzas: {
-    textAlign: 'center',
-    backgroundColor: '#DDD',
-    marginTop: 15,
-    fontSize: 25
+    fontSize: 30, 
   }
 });
