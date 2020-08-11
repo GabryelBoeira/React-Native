c152e5278c1af062692ff295cd3b711764bce4ec
gabryel.boeira <gabryel.boeira@systemglass.com.br>
2020-08-11 09:15:57 -0300

    Trabalhando com Picker
    Utilização de Picker, sempre deve ser importado do 'react-native-community/picker' pois o proprio sistema avisa que o piker padrão esta em descontinuado

--- modulo-intermediario/projetoBase/App.js
+++ modulo-intermediario/projetoBase/App.js
@@ -1,25 +1,44 @@
 import React, { Component } from 'react'
-import { StyleSheet, View, FlatList} from 'react-native'
-import Person from './src/Person/PersonComponent'
+import { Text, StyleSheet, View} from 'react-native'
+import {Picker} from '@react-native-community/picker'
 
 export default class App extends Component {
-  
-  constructor(props) {
+  constructor(props){
     super(props);
-    this.state = {  
-      list: require('./src/pessoas.json'),
-    }
+    this.state = {
+      pizza: 0,
+      pizzas: [{key: 0, value: 29.99, label: "Frango/Catupiry"},
+               {key: 1, value: 34.99, label: "Aliche" },
+               {key: 2, value: 24.99, label: "Calabresa"},
+               {key: 3, value: 29.99, label: "Bacon e Milho"}]
+    };
+
+    this.pizzasItem = this.pizzasItem.bind(this);
   }
 
+  pizzasItem = () => {
+    return(
+     this.state.pizzas.map((item, index) => {
+      return <Picker.Item key={index} value={index} label={item.label} />;
+    }));
+  }
   render() {
+
     return (
       <View style={styles.container}>
-        <FlatList 
-          data={this.state.list}
-          keyExtractor={(item) => item.cpf}
-          renderItem={({item}) => <Person data={item} />}
-          showsVerticalScrollIndicator={false}
-        />
+        <Text style={styles.logo}>Menu Pizza</Text>
+        <Picker
+            selectedValue={this.state.pizza}
+            onValueChange={pizza => this.setState({ pizza })}>
+            {this.pizzasItem()}                      
+        </Picker>
+
+        <Text style={styles.pizzas}>
+          Você Selecionou: {this.state.pizzas[this.state.pizza].label}
+        </Text>
+        <Text style={styles.pizzas}>
+          R$:  {this.state.pizzas[this.state.pizza].value.toFixed(2)}
+        </Text>
       </View>
     );
   }
@@ -28,5 +47,16 @@ export default class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    marginTop: 15
   },
+  logo: {
+    textAlign: 'center',
+    fontSize: 25, 
+  },
+  pizzas: {
+    textAlign: 'center',
+    backgroundColor: '#DDD',
+    marginTop: 15,
+    fontSize: 25
+  }
 });
