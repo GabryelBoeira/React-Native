303817187882abdff1828a3c4c7059e7a91cdd7f
Gabryel Boeira <gabryel392@gmail.com>
2020-09-06 20:20:18 -0300

    app desafio concluido

--- modulo-intermediario/appDesafio/App.js
+++ modulo-intermediario/appDesafio/App.js
@@ -1,32 +1,52 @@
 import React, {Component} from 'react';
-import {TextInput, StyleSheet, View, Text, Switch} from 'react-native';
+import {TextInput, StyleSheet, View, Text, Switch, Button} from 'react-native';
+import {Picker} from '@react-native-community/picker';
+
+const initialState = {
+  nome: null,
+  idade: null,
+  sexo: null,
+  limite: null,
+  isEstudande: false,
+};
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      nome: '',
-      idade: '',
-      sexo: '',
-      limite: '',
-      isEstudande: false,
-    };
-
+    this.state = {...initialState}
     this._isValid = this._isValid.bind(this);
   }
 
   _isValid() {
-    for (let prop in this.state) {
-      if (obj.hasOwnProperty(prop)) alert('Campos não preenchidos!');
-      return false;
+    if(!this.state.nome){
+      alert("Nome não preenchido")
+      return;
     }
-    return true;
+
+    if(!this.state.idade){
+      alert("Idade não preenchido")
+      return;
+    }
+
+    if(!this.state.sexo){
+      alert("Sexo não preenchido")
+      return;
+    }
+
+    if(!this.state.limite){
+      alert("Limite não preenchido")
+      return;
+    }
+    this.setState({...initialState})
+    alert("Conta criada com sucesso!")
   }
 
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.text}>Cadastro Bancario no React Bank of America</Text>
+        <View style={styles.viewTitle}>
+          <Text style={styles.text}>Cadastro Bancario no React Bank of America</Text>
+        </View>        
         <View style={styles.viewColumn}>
           <Text style={styles.text}>Nome: </Text>
           <TextInput
@@ -34,15 +54,32 @@ class App extends Component {
             onChangeText={(nome) => this.setState({nome})}
             value={this.state.nome}
           />
-        </View>
-        <View style={styles.viewColumn}>
-          <Text style={styles.text}>Idade:</Text>
+          <Text style={styles.text}>Limite da Conta:</Text>
           <TextInput
             keyboardType={'number-pad'}
             style={styles.input}
+            onChangeText={(limite) => this.setState({limite})}
+            value={this.state.limite}
+          />
+          <Text style={styles.text}>Idade:</Text>
+          <TextInput
+            keyboardType={'number-pad'}
+            style={{height: 40, width: '100%'}}
             onChangeText={(idade) => this.setState({idade})}
             value={this.state.idade}
           />
+          <Text style={styles.text}>Sexo:</Text>
+          <Picker
+            style={styles.input}
+            selectedValue={this.state.sexo}
+            onValueChange={(sexo) =>
+              this.setState({sexo})
+            }>
+            <Picker.Item label="Selecione" value={null} />
+            <Picker.Item label="Masculino" value={"1"} />
+            <Picker.Item label="Feminino" value={"2"} />
+            <Picker.Item label="Prefiro não Dizer" value={"3"} />
+          </Picker>
         </View>
         <View style={styles.viewRow}>
             <Text style={styles.text}>Estudante: </Text>
@@ -50,6 +87,13 @@ class App extends Component {
                 value={this.state.isEstudande}
                 onValueChange={isEstudande => this.setState({isEstudande})}
             />
+        </View>
+        <View style={styles.viewBtn}>
+          <Button
+            style={styles.btn}
+            onPress={this._isValid}
+            title={"Criar Conta"}
+          />      
         </View>
       </View>
     );
@@ -62,6 +106,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     flexDirection: 'column',
+    
   },
   input: {
     height: 40,
@@ -86,6 +131,23 @@ const styles = StyleSheet.create({
   },
   text: {
     marginBottom: 5,
-    fontSize: 15,
+    fontSize: 19,
+    fontWeight: 'bold',
+  },
+  viewTitle: {
+    alignItems: 'center'
+  },
+  viewBtn: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  btn: {
+    width: 250,
+    height: 50,
+    borderWidth: 2,
+    borderColor: '#dd7b2f',
+    borderRadius: 25
   },
 });
