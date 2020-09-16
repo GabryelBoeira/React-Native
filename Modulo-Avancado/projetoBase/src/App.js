d2127d2c4821aeb6c47e4fb425cb3fa79d053934
gabryel.boeira <gabryel.boeira@systemglass.com.br>
2020-09-16 13:56:53 -0300

    Utilizando Axios
    Montando lista de Filmes em cartaz com a utilização do axios para realizar a consulta do mesmo, permite a utilização simples dos metodos de requisições utilizados na web

---
+++ Modulo-Avancado/projetoBase/src/App.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react'
+import { View, StyleSheet, FlatList, ActivityIndicator } from 'react-native'
+
+import api from './services/api'
+import Movies from './card/Movies.js'
+
+export default class App extends Component {
+
+  state = {
+    movies: [],
+    loading: true
+  }
+
+  async componentDidMount(){
+    const response = await api.get('/r-api/?api=movies')
+    this.setState({
+      movies: response.data,
+      loading: false
+    })
+  }
+
+  render() {
+    if(this.state.loading){
+      return(
+        <View style={styles.viewLoading}>
+          <ActivityIndicator color="#09A6FF" size={40}/>
+        </View>
+      )
+    }else{
+      return(
+        <View style={styles.container}>  
+          <FlatList
+            data={this.state.movies}
+            keyExtractor={item => `${item.id}`}
+            renderItem={ ({item}) => <Movies data={item} /> }
+            
+          />  
+        </View>
+      )
+    }
+  }
+}
+
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+  },
+  viewLoading: {
+    flex: 1,
+    alignItems: 'center', 
+    justifyContent: 'center', 
+  }
+})
---
+++ Modulo-Avancado/projetoBase/src/card/Movies.js
@@ -0,0 +1,61 @@
+import React, {Component} from 'react'
+import {View, Text, StyleSheet, Image, Pressable} from 'react-native'
+
+export default class Movies extends Component{
+  render(){
+    const {nome, foto} = this.props.data
+    return(
+      <View style={styles.card}>
+        <Text style={styles.title}>{nome}</Text>
+        <Image
+          source={{ uri: foto }}
+          style={styles.poster}
+        />
+        <View style={styles.viewButton}>
+          <Pressable style={styles.button} onPress={()=> alert(nome)}>
+            <Text style={styles.buttonText}>LEIA MAIS</Text>
+          </Pressable>
+        </View>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  card: {
+    margin: 15,
+    elevation: 3,
+    shadowRadius: 5,
+    borderRadius: 5,
+    shadowOpacity: 0.8,
+    shadowColor: '#000',
+    backgroundColor: '#FFF',
+    shadowOffset: {width:0, height: 1},
+  },
+  title: {
+    padding: 15,
+    fontSize: 18,
+  },
+  poster: {
+    zIndex: 2,
+    height: 250,
+  },
+  viewButton: {
+    zIndex: 9,
+    marginTop: -40,
+    alignItems: 'flex-end',
+  },
+  button: {
+    width: 100,
+    opacity: 1,
+    padding: 8,
+    borderTopLeftRadius: 5,
+    borderBottomLeftRadius: 5,
+    backgroundColor: '#09A6FF',
+
+  },
+  buttonText: {
+    color: '#FFF',
+    textAlign: 'center',
+  }
+})
