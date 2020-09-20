fd209fa16aecb74c136c92c50298260629588fbd
Gabryel Boeira <gabryel392@gmail.com>
2020-09-19 23:35:40 -0300

    Utilizando animações em sequencia e paralela

--- modulo-animacoes/projetoBase/src/App.js
+++ modulo-animacoes/projetoBase/src/App.js
@@ -5,35 +5,103 @@ export default class App extends Component {
   
   state = {
     widthAnimation: new Animated.Value(150),
-    heightAnimation:  new Animated.Value(50) 
-    
+    heightAnimation:  new Animated.Value(50),
+    opacityAnimation: new Animated.Value(0)
   };
 
   componentDidMount(){     
-
+    /* Animação unica 
     Animated.timing(
       this.state.widthAnimation, {
         toValue: 300,
-        duration: 6000,
+        duration: 2000,
         useNativeDriver: false
       }
     ).start()  
-    Animated.timing(
-      this.state.heightAnimation, {
-        toValue: 300,
-        duration: 6000,
-        useNativeDriver: false
-      }
-    ).start()  
+    
+    Animação em sequecia
+    Animated.sequence([
+      Animated.timing(
+        this.state.heightAnimation, {
+          toValue: 300,
+          duration: 2000,
+          useNativeDriver: false
+        }
+      ),
+      Animated.timing(
+        this.state.widthAnimation, {
+          toValue: 300,
+          duration: 2000,
+          useNativeDriver: false
+        }
+      )
+    ]).start()  
+
+    /* Animação em Paralelo
+    Animated.parallel([
+      Animated.timing(
+        this.state.heightAnimation, {
+          toValue: 300,
+          duration: 2000,
+          useNativeDriver: false
+        }
+      ),
+      Animated.timing(
+        this.state.widthAnimation, {
+          toValue: 300,
+          duration: 2000,
+          useNativeDriver: false
+        }
+      )
+    ]).start()  
+    */
+    // Animação em Paralelo e Sequecia
+    Animated.sequence([
+      Animated.timing(
+        this.state.opacityAnimation, {
+          toValue: 1,
+          duration: 1500,
+          useNativeDriver: false
+        }
+      ),
+      Animated.parallel([
+        Animated.timing(
+          this.state.heightAnimation, {
+            toValue: 300,
+            duration: 2000,
+            useNativeDriver: false
+          }
+        ),
+        Animated.timing(
+          this.state.widthAnimation, {
+            toValue: 300,
+            duration: 2000,
+            useNativeDriver: false
+          }
+        )
+      ]),
+      Animated.timing(
+        this.state.opacityAnimation, {
+          toValue: 0,
+          duration: 1500,
+          useNativeDriver: false
+        }
+      ),
+    ]).start()  
   }
   
   render() {
     return (
       <View style={styles.container}>
 
-        <Animated.View useNativeDriver={false} style={[
-          { width: this.state.widthAnimation, height: this.state.heightAnimation},
-          styles.viewAnimation]}
+        <Animated.View 
+          style={[{ 
+            width: this.state.widthAnimation, 
+            height: this.state.heightAnimation,
+            opacity: this.state.opacityAnimation
+          }, 
+            styles.viewAnimation
+          ]}
         >
           <Text style={styles.textLoading}> Carregando... </Text>  
         </Animated.View>
