6164898e84376ec12bf9acdd099a3e3f835e0b05
Gabryel Boeira <GabryelBoeira@users.noreply.github.com>
2020-10-08 10:14:04 -0300

    Disparanto Aminações Com botões

--- modulo-animacoes/projetoBase/src/App.js
+++ modulo-animacoes/projetoBase/src/App.js
@@ -1,68 +1,91 @@
-import React, { Component } from 'react'
-import { Text, StyleSheet, View, Animated} from 'react-native'
+import React, {Component} from 'react';
+import {Text, StyleSheet, View, Animated, Pressable} from 'react-native';
 
 export default class App extends Component {
-  
   state = {
     widthAnimation: new Animated.Value(150),
-    heightAnimation:  new Animated.Value(100),
+    heightAnimation: new Animated.Value(35),
+    opacityAnimation: new Animated.Value(0),
   };
 
-  componentDidMount(){     
- 
-    Animated.loop(
-      Animated.sequence([
-        Animated.timing(
-        this.state.heightAnimation, {
-          toValue: 300,
-          duration: 700,
-            useNativeDriver: false
-          }
-        ),
-        Animated.timing(
-          this.state.heightAnimation, {
-            toValue: 100,
-            duration: 700,
-              useNativeDriver: false
-            }
-          )        
-      ])
-    ).start()  
-  }
-  
+  carregarGrafico = () => {
+    Animated.sequence([
+      Animated.timing(this.state.opacityAnimation, {
+        toValue: 1,
+        duration: 100,
+        useNativeDriver: false,
+      }).start(),
+      Animated.timing(this.state.heightAnimation, {
+        toValue: 300,
+        duration: 1000,
+        useNativeDriver: false,
+      }).start(),
+    ]);
+  };
+
   render() {
     return (
       <View style={styles.container}>
+        <View style={styles.viewPressable}>
+          <Pressable onPress={() => this.carregarGrafico()}>
+            <Text style={styles.pressableText}>Gerar Grafico</Text>
+          </Pressable>
+        </View>
 
-        <Animated.View 
-          style={[{ 
-            width: this.state.widthAnimation, 
-            height: this.state.heightAnimation,
-          }, 
-            styles.viewAnimation
-          ]}
-        >
-          <Text style={styles.textLoading}> Carregando... </Text>  
-        </Animated.View>
-
+        <View style={styles.viewGrafico}>
+          <Animated.Text
+            style={({opacity: this.state.opacityAnimation}, styles.textTitle)}>
+            Vendas
+          </Animated.Text>
+          <Animated.View
+            style={[
+              {
+                width: this.state.widthAnimation,
+                height: this.state.heightAnimation,
+                opacity: this.state.opacityAnimation,
+              },
+              styles.viewAnimation,
+            ]}>
+            <Text style={styles.textLoading}> R$ 150,00 </Text>
+          </Animated.View>
+        </View>
       </View>
-    )
+    );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center'
   },
-  viewAnimation: {   
+  viewAnimation: {
     backgroundColor: '#4169e1',
-    justifyContent: 'center'
+    justifyContent: 'center',
   },
   textLoading: {
     fontSize: 22,
     color: '#fff',
-    textAlign: 'center'
-  }
-})
+    textAlign: 'center',
+  },
+  viewPressable: {
+    height: 80,
+    alignItems: 'center',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    backgroundColor: '#4169e1',
+  },
+  pressableText: {
+    fontSize: 25,
+    color: '#fff',
+  },
+  viewGrafico: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  textTitle: {
+    fontSize: 20,
+    color: '#000',
+  },
+});
